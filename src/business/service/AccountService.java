06f2fa49d3550f65c96e2e626669d6010d8e2d58
nealecon96 <16192206@studentmail.ul.ie>
2019-12-01 13:13:34 +0000

    Handler to deal with strings, service the rest

--- src/business/service/AccountService.java
+++ src/business/service/AccountService.java
@@ -9,7 +9,29 @@ public class AccountService {
     
     ArrayList<Account> accounts;
     ArrayList<Integer> phoneNumbers;
-    AccountHandlerSingleton handler = new AccountHandlerSingleton();
+    AccountHandlerSingleton handler = AccountHandlerSingleton.getInstance();
+    
+    public ArrayList<Account> getAccounts()
+    {
+        ArrayList<String> lines = handler.getData();
+        ArrayList<Account> accounts = new ArrayList<>();
+        String lineFromFile, forename, surname, phoneNumber;
+	String[] split;
+	Account anAccount;
+
+        for(String i: lines)
+        {
+            lineFromFile = i;
+            split = lineFromFile.split(",");
+            forename = split[0];
+            surname = split[1];
+            phoneNumber = split[2];
+            anAccount = new Account(Integer.parseInt(phoneNumber), forename, surname);
+            accounts.add(anAccount);
+	}	
+		
+	return accounts;	
+    }
     
     public boolean validName(String name)
     {
@@ -32,20 +54,55 @@ public class AccountService {
         
         return valid;
     }
+     
+    public ArrayList<Integer> getPhoneNumbers() throws FileNotFoundException
+    {
+            
+        ArrayList<String> lines = handler.getData();
+        ArrayList<Integer> phoneNumbers = new ArrayList<>();
+        String lineFromFile;
+	String[] split;
+	
+        for(String i: lines)
+        {
+            lineFromFile =i;
+            split = lineFromFile.split(",");
+            phoneNumbers.add(Integer.parseInt(split[2]));
+        }
+		
+	return phoneNumbers;
+    }
     
     public boolean alreadyRegistered(int phoneNumber) throws FileNotFoundException
     {
         boolean valid = false;
-        phoneNumbers = handler.getPhoneNumbers();
+        phoneNumbers = getPhoneNumbers();
         
         if(phoneNumbers.indexOf(phoneNumber) >= 0)
+        {
             valid = true;
+        }
 
         return valid;
+        
     }
     
-    public void registerAccount(Account anAccount) throws FileNotFoundException
+    
+    
+    public void registerAccount(Account account)
     {
-        handler.addAccount(anAccount);
+        boolean old = false;
+        
+        ArrayList<Account> accounts = getAccounts();
+        accounts.add(account);
+        ArrayList<String> lines = new ArrayList<>();
+        
+        for(Account i: accounts)
+        {
+            lines.add(i.toString());
+        }
+        
+        handler.updateData(lines);
     }
+    
 }
--- src/data/AccountHandlerSingleton.java
+++ src/data/AccountHandlerSingleton.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AccountHandlerSingleton implements IDataHandler{
     
@@ -28,76 +30,43 @@ public class AccountHandlerSingleton implements IDataHandler{
 
     }
     
-    public ArrayList<Integer> getPhoneNumbers() throws FileNotFoundException
+    @Override
+    public ArrayList<String> getData()
     {
-        
-        
         File con = connector.getConnection();
         
-        ArrayList<Integer> phoneNumbers = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
         String lineFromFile;
-	String[] split;
-	
-	Scanner in = new Scanner(con);
-        while(in.hasNext())
-        {
-            lineFromFile = in.nextLine();
-            split = lineFromFile.split(",");
-            phoneNumbers.add(Integer.parseInt(split[2]));
+        try {
+            Scanner in = new Scanner(con);
+            while(in.hasNext())
+            {
+                lineFromFile = in.nextLine();
+                strings.add(lineFromFile);
+            }   
+            in.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(OrderHandlerSingleton.class.getName()).log(Level.SEVERE, null, ex);
         }
-        in.close();
-		
-	return phoneNumbers;
+        return strings;
     }
     
-    public ArrayList<Account> getAccounts() throws FileNotFoundException
-    {
-        ArrayList<Account> accounts = new ArrayList<>();
-        String lineFromFile, forename, surname, phoneNumber;
-	String[] split;
-	Account anAccount;
-	File con = connector.getConnection();
-	Scanner in = new Scanner(con);
-        while(in.hasNext())
-        {
-            lineFromFile = in.nextLine();
-            split = lineFromFile.split(",");
-            forename = split[0];
-            surname = split[1];
-            phoneNumber = split[2];
-            anAccount = new Account(Integer.parseInt(phoneNumber), forename, surname);
-            accounts.add(anAccount);
-	}	
-        in.close();
-		
-	return accounts;	
-    }
-    
-    public void addAccount(Account anAccount) throws FileNotFoundException 
-    {
-        ArrayList<Account> accounts = getAccounts();
-        accounts.add(anAccount);
-        writeAccounts(accounts);
-    }
-    
-    public void writeAccounts(ArrayList<Account> accounts) throws FileNotFoundException
+    @Override
+    public void updateData(ArrayList<String> lines)
     {	
         File con = connector.getConnection();
-        PrintWriter OutFile = new PrintWriter(con);
-	for(Account i: accounts)
-	{
-            OutFile.println(i.toString());
-	}
-	OutFile.close();
-    }
-
-    @Override
-    public ArrayList<String> getData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void updateData(ArrayList<String> strings) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        PrintWriter OutFile = null;
+        try {
+            OutFile = new PrintWriter(con);
+            for(String i: lines)
+            {
+                OutFile.println(i.toString());
+            }   OutFile.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(OrderHandlerSingleton.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            OutFile.close();
+        }
     }
 }
